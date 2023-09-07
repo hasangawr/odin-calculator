ffe8dd5b5c508762daec9fa78ae3061690665910
hasangawr <hasangawr@gmail.com>
2023-09-07 06:57:15 +0530

    Create file structure & basic math functions

---
+++ index.js
@@ -0,0 +1,19 @@
+let firstNum;
+let secondNum;
+let operator;
+
+const add = function(firstNum, secondNum) {
+    return firstNum + secondNum;
+}
+
+const subtract = function(firstNum, secondNum) {
+    return firstNum - secondNum;
+}
+
+const multiply = function(firstNum, secondNum) {
+    return firstNum * secondNum;
+}
+
+const divide = function(firstNum, secondNum) {
+    return firstNum / secondNum;
+}
